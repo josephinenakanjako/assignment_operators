2535dcdd9a478b2a1f40be5a2059101f30902002
josephinenakanjako <143077466+josephinenakanjako@users.noreply.github.com>
2023-09-12 01:24:55 -0700

    Add files via upload

---
+++ exercise.py
@@ -0,0 +1,49 @@
+# Even NUMBERS
+EvenNumbers=(2,4,6)
+TotalEvenNumber=2+4+6
+print("The sum of even numbers less than 8is" +str(TotalEvenNumber))
+print(TotalEvenNumber)
+
+# The total number of eggs
+Eggs=3,5,6,8,9
+TotalEggs =3+5+6+8+9
+Average=(TotalEggs)/5
+print (Average)
+
+
+-5+8*6
+j=-5
+k=8
+l=6
+m=k*l
+print(j+m)
+
+
+(55+9)%9
+e=55
+f=9
+g=9
+h=(e+f)
+print(h%g)
+
+
+20+-3 * 5 / 8
+o=20
+p=-3
+q=5
+r=8
+s=(p*q)/r
+print(o+(s))
+
+
+
+5+15/3*2-8%3
+p=5
+i=15
+n=3
+e=3
+j=2
+o=-8
+s=3
+e=(5+(15/3)*2)-8
+print(e%s)
